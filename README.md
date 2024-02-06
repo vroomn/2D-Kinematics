1bad1c4a6103f90d006f18bbb3105a423e2a19c6
Vroom <harrison.hilpert@gmail.com>
2024-02-06 12:10:47 -0800

    Pygame Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# 2D Kinematics Simulator
+A simple simulation of an object flying out of a cannon at a specified velocity and angle
+
+## Running the software (or appending more)
+I reccomend using python virtual enviroments  
+```python -m venv 'YOUR NAME CHOICE'```  
+Then navigate to the "Scripts" directory and run either "Activate.ps1" or "activate.bat". Now that it's a virtual enviroment the project requries Pygame, so:  
+```pip install pygame```
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Hello World')
+import pygame
+
+# Width of the simulator window
+WIDTH = 1280
+# Height of the simulator window
+HEIGHT = 720
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+displaying = True
+while displaying:
+    # Input and general event handler
+    for event in pygame.event.get():
+        # If the program is shut down
+        if event.type == pygame.QUIT:
+            displaying = False
+
+    screen.fill((121, 125, 133))
+
+    pygame.display.flip()
+    # Delta time between frames
+    dt = clock.tick(60) / 1000 # tick locks fps to 60
+
+pygame.quit()
