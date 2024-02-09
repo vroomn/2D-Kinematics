d7de766faf6cbf785a4282b7b568158297e17bfa
Vroom <harrison.hilpert@gmail.com>
2024-02-09 06:06:10 -0800

    Base Lob Code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import pygame.gfxdraw
+import math
 
 # Width of the simulator window
 WIDTH = 1280
@@ -8,6 +10,43 @@ HEIGHT = 720
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+
+class PhysicsObject:
+    def __init__(self, initVelocity: int, launchAngle: int, gravity: int, radius: int, color: tuple) -> None:
+        # Variables for physics sim
+        self.initVelocity = initVelocity
+        self.launchAngle = launchAngle
+        self.clampLaunchAngle(90)
+        self.gravity = gravity
+        self.dStartTime = 0
+
+        # Rendering variables
+        self.radius = radius
+        self.color = color
+        self.position = pygame.Vector2(100, 100)
+
+    def clampLaunchAngle(self, maxAngle) -> None:
+        if self.launchAngle > maxAngle: self.launchAngle = maxAngle
+        elif self.launchAngle < 0: self.launchAngle = 0
+
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.gfxdraw.filled_circle(screen, int(self.position.x), int(self.position.y), self.radius, (self.color))
+
+    def physicsStep(self, tick) -> None:
+        time = (pygame.time.get_ticks() - tick) / 1000
+
+        # Y component
+        self.position.y = screen.get_height() - ( ((self.initVelocity*math.sin(math.radians(self.launchAngle)))*time) + (.5)*(self.gravity)*(time)**2 )*5
+
+        # X Component
+        self.position.x = ((self.initVelocity*math.cos(math.radians(self.launchAngle)))*time)*5
+        pass
+
+physObject = PhysicsObject(50, 20, -10, 10, (255, 255, 255))
+
+dt = 0
+
+initTick = pygame.time.get_ticks()
 
 displaying = True
 while displaying:
@@ -19,50 +58,14 @@ while displaying:
 
     screen.fill((121, 125, 133))
 
+    if physObject.position.y < screen.get_height():
+        physObject.physicsStep(initTick)
+
+    physObject.draw(screen)
+
     pygame.display.flip()
     # Delta time between frames
     dt = clock.tick(60) / 1000 # tick locks fps to 60
 
-#Drawing objects
-ball = pygame.draw.circle (screen, pygame.Color(255, 0, 0, 255), (60, 110), 20 )
- 
-
-#variables
-gravity = 1.0
-
-
-#circle class 
-class Ball:
-    def __init__(self,acceleration, angle, x_initialvel, y_initialvel, x_finalvel, y_finalvel, y_position, x_position):
-        self.accel = acceleration
-        self.angle = angle
-        self.initial_x = x_initialvel
-        self.initial_y = y_initialvel
-        self.x_final = x_finalvel
-        self.y_final = y_finalvel
-        self.y_pos = y_position
-        self.x_pos = x_position
-
-#drawing the circle
-        def draw_circle(self):
-            pygame.draw.circle (screen, pygame.Color(255, 0, 0, 255), (60, 110), 20 )
-            
-#gravity function
-#def if_falling ():
-    
-    
-
-
-angle_check = False
-
-while not angle_check: 
-    def less_than():
-        angle = int(input("Please enter the angle you want the ball to be fired at:")) 
-        if angle > 90:
-            return False
-        else: 
-            True
-    
-    
-    pygame.quit()
+pygame.quit()
 
