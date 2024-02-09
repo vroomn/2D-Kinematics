812dfd2f38146336093fa8c87ec8f4ed361a5f17
Vroom <harrison.hilpert@gmail.com>
2024-02-09 07:10:53 -0800

    Main Cleanup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import ui_system
 
 # Width of the simulator window
 WIDTH = 1280
@@ -23,20 +22,5 @@ while displaying:
     pygame.display.flip()
     # Delta time between frames
     dt = clock.tick(60) / 1000 # tick locks fps to 60
-
-#valid_grav = False
-#gravity = 0
-#while not valid_grav:
- #   gravity = abs(int(input("Enter a valid integer for the value of the gravitational pull:")))
-  #  print (gravity)
-   # if gravity < 0:
-    #    valid_grav = True
-
-#valid_speed = False
-#initial_speed = 0
-#while not valid_speed:
- #   initial_speed = abs(int(input("Enter a valid integer for the value of the object's speed:")))
-  #  if initial_speed >= 1:
-   #     valid_speed = True
 
 pygame.quit()
